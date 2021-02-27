fa5020c9fee999598edd0c3cf14adfc9aa0f0511
Jan Schering <jan.schering@blueonic.com>
2021-02-27 10:04:33 +0100

    Image support + background-image for landing page

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const isDevelopment = process.env.NODE_ENV === "development";
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 module.exports = {
   entry: {
@@ -50,6 +51,27 @@ module.exports = {
             loader: "sass-loader",
             options: {
               sourceMap: isDevelopment
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: "file-loader" // Or `url-loader` or your other loader
+          },
+          {
+            loader: ImageMinimizerPlugin.loader,
+            options: {
+              severityError: "warning", // Ignore errors on corrupted images
+              minimizerOptions: {
+                plugins: [
+                  ["gifsicle", { interlaced: true }],
+                  ["jpegtran", { progressive: true }],
+                  ["optipng", { optimizationLevel: 5 }]
+                ]
+              }
             }
           }
         ]
