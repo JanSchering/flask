9d1ca0177706a857430c40c2615538c01956d7dd
Jan Schering <jan.schering@blueonic.com>
2021-03-06 12:43:37 +0100

    styling, mdxProviders

--- makefile
+++ makefile
@@ -1,4 +1,5 @@
 build:
+	export NODE_ENV="development"
 	rm -R $(shell pwd)/app/static/bundles
 	npm run dev
 	flask run
--- react/pages/home/app.jsx
+++ react/pages/home/app.jsx
@@ -7,26 +7,44 @@ import "../../../sass/global";
 import Home from "../../../mdx/Home.mdx";
 import Projects from "../../../mdx/Projects.mdx";
 import { Navbar } from "../../components/navbar";
+import { MDXProvider } from "@mdx-js/react";
+import { MDBJumbotron, MDBContainer } from "mdbreact";
+
+const Wrapper = props => {
+  console.log(props);
+
+  return (
+    <MDBJumbotron fluid>
+      <MDBContainer>
+        <main {...props} />
+      </MDBContainer>
+    </MDBJumbotron>
+  );
+};
 
 export const App = () => {
   return (
     <BrowserRouter history={history}>
       <Navbar />
-      <Switch>
-        <Route exact path={["/", "/index", "/home"]}>
-          <Home />
-        </Route>
-        <Route exact path="/projects">
-          <Projects />
-        </Route>
-        <Route
-          path="/test"
-          component={() => {
-            window.location.href = "https://www.github.com";
-            return null;
-          }}
-        />
-      </Switch>
+      <div className="pageContainer">
+        <Switch>
+          <Route exact path={["/", "/index", "/home"]}>
+            <MDXProvider components={{ wrapper: Wrapper }}>
+              <Home />
+            </MDXProvider>
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+          <Route
+            path="/test"
+            component={() => {
+              window.location.href = "https://www.github.com";
+              return null;
+            }}
+          />
+        </Switch>
+      </div>
     </BrowserRouter>
   );
 };
